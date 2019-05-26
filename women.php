13983ff10c42491c818d3dcb8d7c8ef50373283a
Stina LUTZENKIRCHEN <sluetzen@e1r11p11.42.fr>
2019-05-26 16:30:50 +0200

    modifications in css & local headers/footers

--- women.php
+++ women.php
@@ -2,57 +2,10 @@
 <html>
 	<head>
 		<title>WEBSITE</title>
-		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 	<body >
-		<div id="menu">
-            <div id="left">
-			    <div class="dropdown">
-				<button class="button darkblue">WOMEN</button>
-				<div class="dropdown-content darkblue">
-					<a href="#"></a>
-					<a href="#">Clothes</a>
-					<a href="#">Shoes</a>
-					<a href="#">Accessoires</a>
-				</div>
-			    </div>
-			    <div class="dropdown">
-				<button class="button seablue">MEN</button>
-				<div class="dropdown-content seablue">
-					<a href="#">Trousers</a>
-					<a href="#">Shirts</a>
-					<a href="#">Shoes</a>
-					<a href="#">Underwear</a>
-				</div>
-			    </div>
-			    <div class="dropdown">
-				<button class="button middleblue">CHILDREN</button>
-				<div class="dropdown-content middleblue">
-					<a href="#">Clothes</a>
-					<a href="#">Shoes</a>
-					<a href="#">Accessoires</a>
-				</div>
-                </div>
-            </div>
-            <div id="middle">
-                <h1><a class="homepage" href="index.php">NAME OF WEBSITE</a></h1>
-            </div>
-            <div id="push">
-			    <div class="dropdown">
-				<button class="button lightblue">BASKET</button>
-				<div class="dropdown-content lightblue">
-					<a href="#">See your basket</a>
-					<a href="#">Empty basket</a>
-					<a href="#">Checkout</a>
-				</div>
-			    </div>
-			    <div class="dropdown">
-				<button class="button verylightblue">USERPAGE</button>
-				<div class="dropdown-content verylightblue">
-				</div>
-                </div>
-            </div>
-		</div>
+	<?php require_once 'header.php';  ?>
 		<br>
 		<div id="center">
 			<div class="row">
